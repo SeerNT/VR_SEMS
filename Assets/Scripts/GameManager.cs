7b36882fde91ac4c2ae6a4b15d24a3643e57145b
SeerNT <63809506+SeerNT@users.noreply.github.com>
2023-10-17 21:55:55 +0300

    Sem 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Button changeBut;
     [SerializeField] private GameObject capsule;
 
+    [SerializeField] private TextMeshProUGUI colCounter;
+
+    private int lives = 10;
 
     void Start()
     {
@@ -17,5 +21,15 @@ public class GameManager : MonoBehaviour
     void ChangeState()
     {
         capsule.SetActive(!capsule.activeSelf);
+    }
+
+    public void DecreaseLives()
+    {
+        lives--;
+    }
+
+    void Update()
+    {
+        colCounter.text = "Collision Lives: " + lives.ToString();
     }
 }
